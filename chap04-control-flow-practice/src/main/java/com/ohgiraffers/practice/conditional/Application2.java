0e8b4aaf4b26d2fe958458d990c8f7b13e82a88a
pyunghun <raccoon.ohgiraffers@gmail.com>
2024-09-03 16:13:21 +0900

    [java-looping-practice]

---
+++ chap04-control-flow-practice/src/main/java/com/ohgiraffers/practice/conditional/Application2.java
@@ -0,0 +1,23 @@
+package com.ohgiraffers.practice.conditional;
+
+public class Application2 {
+
+    public static void main(String[] args) {
+
+        /* 람쥐가 이벤트 부스에서 짝홀 게임 진행을 맡았습니다.
+        *
+         * "짝홀, 그 결과는?"을 출력하고, 도토리 갯수를 정수로 입력받아
+         * 그 수가 짝수인지 홀수인지 판단해서 결과를 출력하세요.
+         *
+         * 짝수인 경우 "짝수입니다~!" 출력, 홀수인 경우 "홀수입니다~!" 출력
+         *
+         * -- 입력 예시 --
+         * 짝홀, 그 결과는? 7
+         *
+         * -- 출력 예시 --
+         * 홀수입니다~!
+         * */
+
+    }
+
+}
---
+++ chap04-control-flow-practice/src/main/java/com/ohgiraffers/practice/looping/Application6.java
@@ -0,0 +1,18 @@
+package com.ohgiraffers.practice.looping;
+
+public class Application6 {
+
+    public static void main(String[] args) {
+        /* 정수를 입력받아 1부터 입력받은 정수까지
+         * 홀수이면 "토", 짝수이면 "마"가 정수만큼 누적되어 출력되도록 하세요.
+         *
+         * -- 입력 예시 --
+         * 정수를 입력하세요 : 5
+         *
+         * -- 출력 예시 --
+         * 수박수박수
+         * */
+
+    }
+
+}
