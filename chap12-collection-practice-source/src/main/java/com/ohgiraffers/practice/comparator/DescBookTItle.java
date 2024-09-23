803d735eb846fd7268fb32d09e486777455ca210
yejin <jin200126@gmail.com>
2024-09-23 18:25:21 +0900

    240923

--- chap12-collection-practice-source/src/main/java/com/ohgiraffers/practice/comparator/DescBookTItle.java
+++ chap12-collection-practice-source/src/main/java/com/ohgiraffers/practice/comparator/DescBookTItle.java
@@ -8,6 +8,6 @@ public class DescBookTItle implements Comparator<BookDTO> {
 
     @Override
     public int compare(BookDTO o1, BookDTO o2) {
-        return o1.getTitle().compareTo(o2.getTitle());
+        return o2.getTitle().compareTo(o1.getTitle());
     }
 }
